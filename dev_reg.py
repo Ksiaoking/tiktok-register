19f335911b6f1f26dbdd7eee135e4a04b8bec268
ksiaoking <102038338+Ksiaoking@users.noreply.github.com>
2026-07-22 11:04:47 +0300

    Create dev_reg.py

---
+++ dev_reg.py
@@ -0,0 +1,99 @@
+import random
+import time
+import json
+from collections import OrderedDict
+#---------------------------------
+# tg: dacent53
+# These methods have not been changed by our tools.
+# curl_init
+# curl_setopt
+# curl_exec
+# curl_close
+#----------------------------
+
+mc = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str('' + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + ':') + str(random.randint(1, 9))) + '9:8A:7') + str(random.randint(1, 9))) + ':') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + ':') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '';
+SerialNumber = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str('' + str(random.randint(1, 9))) + 'abd') + str(random.randint(1, 9))) + 'ec') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'f';
+google_aid = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str('cf' + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'ce-') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'b') + str(random.randint(1, 9))) + '-') + str(random.randint(1, 9))) + 'c') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '-') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '1e-ac') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'bfb') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'f') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '';
+clientudid = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str('' + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '';
+openuduid = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str('' + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '';
+osversion = str(str(str(str(str('' + str(random.randint(4, 5))) + '.') + str(random.randint(0, 9))) + '.') + str(random.randint(0, 9))) + '';
+appversion = '9.1.0';
+device_brand = 'samsung';
+devicemodel = 'Galaxy J';
+# $ClientUdid = '59c3962d-202c-4bcd-8150-43db55cc5d15';
+# 34c7115d-643c-7bcd-629
+ClientUdid1 = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(random.randint(1, 9)) + '') + str(random.randint(1, 9))) + 'c') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'd-') + str(random.randint(1, 6))) + '') + str(random.randint(1, 5))) + '') + str(random.randint(1, 9))) + 'c-';
+ClientUdid2 = str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(str(ClientUdid1) + '') + str(random.randint(1, 9))) + 'bcd-') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + '-') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'db') + str(random.randint(1, 9))) + '') + str(random.randint(1, 9))) + 'cc') + str(random.randint(1, 9))) + 'd') + str(random.randint(1, 9))) + str(random.randint(1, 9));
+print("mc: " + str(mc) + "\n",end="");
+print("SerialNumber: " + str(SerialNumber) + "\n",end="");
+print("google_aid: " + str(google_aid) + "\n",end="");
+print("clientudid: " + str(clientudid) + "\n",end="");
+print("openuduid: " + str(openuduid) + "\n",end="");
+print("osversion: " + str(osversion) + "\n",end="");
+# $Data = [
+#
+#     "magic_tag"=>"ss_app_log",
+#     "_gen_time"=>time(),
+#     "header"=> [
+#       "appkey"=>"57bfa27c67e58e7d920028d3",
+#       "udid"=>$clientudid,
+#       "openudid"=>$openuduid,
+#       "sdk_version"=>"2.5.5.8",
+#       "package"=>"com.zhiliaoapp.musically",
+#       "channel"=>"googleplay",
+#       "display_name"=>"musical_ly",
+#       "app_version"=>"13.1.3",
+#       "version_code"=>"130103",
+#       "timezone"=>"3",
+#       "access"=>"wifi",
+#       "os"=>"android",
+#       "os_version"=>"9",
+#       "os_api"=>"28",
+#       "device_model"=>$devicemodel,
+#       "device_brand"=>"HUAWEI",
+#       "device_manufacturer"=>"HUAWEI",
+#       "language"=>"en",
+#       "resolution"=>"1080*2032",
+#       "display_density"=>"191",
+#       "density_dpi"=>"mdpi",
+#       "mc"=>$mc,
+#       "carrier"=>"GSM",
+#       "mcc_mnc"=>"25002",
+#       "clientudid"=>$clientudid,
+#       "sig_hash"=>"aea615ab910015038f73c47e45d21466",
+#       "aid"=>"1233",
+#       "rom"=>"G361HXXU0AQK10",
+#       "release_build"=>"c4e7178_20190401",
+#       "update_version_code"=>"2019091803",
+#       "manifest_version_code"=>"2019091803",
+#       "cpu_abi"=>"armeabi-v7a",
+#       "build_serial"=>"2311671a",
+#       "serial_number"=>"2311671a",
+#       "sim_serial_number"=> ["sim_serial_number"=>"81563787888753294055"],
+#       "not_request_sender"=>"0",
+#       "rom_version"=>"LMY48BA.G361HXXU0AQK10",
+#       "region"=>"RU",
+#       "tz_name"=>"Europe%2FMinsk",
+#       "tz_offset"=>"10800",
+#       "sim_region"=>"RU",
+#       "google_aid"=>$google_aid,
+#       "app_language"=>"en"
+#     ]
+#
+# ];
+# Device-Register Request
+Data = OrderedDict([('header',OrderedDict([('appkey','57bfa27c67e58e7d920028d3'),('serial_number',SerialNumber),('display_density','mdpi'),('tz_name','America/New_York'),('rom','G361HXXU0AQK10'),('release_build','7f052cf_20181116'),('resolution','1794x1080'),('timezone','1'),('carrier','GSM'),('mcc_mnc','20820'),('sim_serial_number',OrderedDict([])),('rom_version','OPM6.171019.030.K1'),('density_dpi','420'),('device_brand',device_brand),('manifest_version_code','2018111632'),('device_manufacturer','LGE'),('clientudid',clientudid),('openudid',openuduid),('update_version_code','2018111632'),('os_api','28'),("display_name","musical_ly"),("app_version","13.1.3"),("version_code","130103"),('mc',mc),('language','ru'),('build_serial',SerialNumber),('device_model',devicemodel),('google_aid',google_aid),('region','RU'),('package','com.zhiliaoapp.musically'),('tz_offset','3600'),('sim_region','ru'),('access','wifi'),('os_version',osversion),('sdk_version','4'),('carrier','Lycamobile'),('cpu_abi','armeabi-v7a'),('aid','1233'),('app_version',appversion),('os','Android'),('not_request_sender',0),('channel','wandoujia')])),('_gen_time',int(time.time())),('magic_tag','ss_app_log')]);
+Encoded = json.dumps(Data);
+ch = curl_init();
+curl_setopt(ch, CURLOPT_URL, "http://applog.musical.ly/service/2/device_register/");
+#Tiktok Device Resigter Parametre
+curl_setopt(ch, CURLOPT_RETURNTRANSFER, True);
+curl_setopt(ch, CURLOPT_USERAGENT, "okhttp/3.10.0.1");
+curl_setopt(ch, CURLOPT_HEADER, True);
+curl_setopt(ch, CURLOPT_POST, 1);
+curl_setopt(ch, CURLOPT_POSTFIELDS, Encoded);
+output = curl_exec(ch);
+curl_close(ch);
+print("\n",end="");
+print(output);
+print("\n",end="");
